e85e6d80119f1ede022d9784ceb11a3f3698fcee
808Code <dikshant.shah2k@gmail.com>
2022-05-17 21:42:32 +0545

    first commit

---
+++ src/main/java/com/bonsai/p2plending/PeerToPeerLendingApplication.java
@@ -0,0 +1,13 @@
+package com.bonsai.p2plending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerToPeerLendingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeerToPeerLendingApplication.class, args);
+    }
+
+}
