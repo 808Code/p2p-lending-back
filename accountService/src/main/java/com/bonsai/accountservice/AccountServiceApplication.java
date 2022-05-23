8171c8ee1058a25c3ed8e87f3bce0a84a84c3842
808Code <dikshant.shah2k@gmail.com>
2022-05-23 18:41:32 +0545

    removed runner

--- accountService/src/main/java/com/bonsai/accountservice/AccountServiceApplication.java
+++ accountService/src/main/java/com/bonsai/accountservice/AccountServiceApplication.java
@@ -1,7 +1,10 @@
 package com.bonsai.accountservice;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class AccountServiceApplication {
