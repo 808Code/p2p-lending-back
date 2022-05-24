a8ff38c43f94a5eaa346f963a65e0a9c12d68eb9
808Code <dikshant.shah2k@gmail.com>
2022-05-24 22:37:46 +0545

    feat SaveEmail

--- accountService/src/main/java/com/bonsai/accountservice/repositories/UserCredentialRepo.java
+++ accountService/src/main/java/com/bonsai/accountservice/repositories/UserCredentialRepo.java
@@ -3,9 +3,10 @@ package com.bonsai.accountservice.repositories;
 import com.bonsai.accountservice.models.UserCredential;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserCredentialRepo extends JpaRepository<UserCredential, UUID> {
-
+    Optional<UserCredential> findByEmail(String email);
 
 }
