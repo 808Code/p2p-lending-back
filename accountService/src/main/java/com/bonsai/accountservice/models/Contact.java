c21f9e7ada789dca45351287fcefc9bcd3f7710c
808Code <dikshant.shah2k@gmail.com>
2022-06-02 23:51:03 +0545

    feat. register kyc

---
+++ accountService/src/main/java/com/bonsai/accountservice/models/Contact.java
@@ -0,0 +1,35 @@
+package com.bonsai.accountservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Builder
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class  Contact{
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+
+    String otherEmail;
+
+    String telephone;
+
+    @Column(nullable = false)
+    String primaryMobile;
+
+    String secondaryMobile;
+
+
+//    @OneToOne(mappedBy = "contact")
+//    private KYC kyc;
+
+}
