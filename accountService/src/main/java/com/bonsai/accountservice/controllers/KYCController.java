778f36ff9390f64146230e62ab323099537a6cab
808Code <dikshant.shah2k@gmail.com>
2022-06-02 23:48:48 +0545

    feat. register kyc

---
+++ accountService/src/main/java/com/bonsai/accountservice/controllers/KYCController.java
@@ -0,0 +1,44 @@
+package com.bonsai.accountservice.controllers;
+
+
+import com.bonsai.accountservice.dto.request.RegisterKYCRequest;
+import com.bonsai.accountservice.dto.response.SuccessResponse;
+import com.bonsai.accountservice.services.RegistrationService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@RequiredArgsConstructor
+@Controller
+@Slf4j
+public class KYCController {
+    private final RegistrationService registrationService;
+
+
+
+    @PostMapping("/registerKYC")
+    public ResponseEntity<SuccessResponse> registerKYC(@ModelAttribute RegisterKYCRequest request){
+        log.info("Request = {}",request);
+        registrationService.saveKYC(request);
+
+        return ResponseEntity.ok(
+                new SuccessResponse("saved",true)
+        );
+    }
+
+//    @PostMapping("/verifyKYC")
+//    public ResponseEntity<SuccessResponse> registerKYC(@ModelAttribute RegisterKYCRequest request){
+//        log.info("Request = {}",request);
+//        registrationService.saveKYC(request);
+//
+//        return ResponseEntity.ok(
+//                new SuccessResponse("saved",true)
+//        );
+//    }
+
+
+}
