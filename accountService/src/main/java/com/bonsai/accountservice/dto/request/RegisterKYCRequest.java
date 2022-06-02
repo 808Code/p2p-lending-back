ad30d73fa18a5f1965e53b94e7633148035ce483
808Code <dikshant.shah2k@gmail.com>
2022-06-02 23:49:24 +0545

    feat. register kyc

---
+++ accountService/src/main/java/com/bonsai/accountservice/dto/request/RegisterKYCRequest.java
@@ -0,0 +1,27 @@
+package com.bonsai.accountservice.dto.request;
+
+import com.bonsai.accountservice.models.Address;
+import com.bonsai.accountservice.models.Contact;
+import com.bonsai.accountservice.models.Finance;
+import org.springframework.web.multipart.MultipartFile;
+
+public record RegisterKYCRequest(
+        String firstName,
+        String middleName,
+        String lastName,
+        String Gender,
+        String citizenShipNumber,
+        String dob,
+        String maritalStatus,
+        String children,
+        String currentlyStudying,
+        String occupation,
+        String temporaryAddress,
+        String permanentAddress,
+        String contact,
+        String finance,
+        MultipartFile profilePhoto,
+        MultipartFile citizenShipPhotoFront,
+        MultipartFile citizenShipPhotoBack
+        ){}
+
