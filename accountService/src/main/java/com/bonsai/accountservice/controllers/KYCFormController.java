6024ebd4058a8d7f577ef2ef3b49b4c8af841e3e
808Code <dikshant.shah2k@gmail.com>
2022-06-06 18:30:45 +0545

    feat.KYC form

--- accountService/src/main/java/com/bonsai/accountservice/controllers/KYCFormController.java
+++ accountService/src/main/java/com/bonsai/accountservice/controllers/KYCFormController.java
@@ -1,8 +1,11 @@
 package com.bonsai.accountservice.controllers;
 
 
+import com.bonsai.accountservice.dto.request.GetKYCRequest;
 import com.bonsai.accountservice.dto.request.RegisterKYCRequest;
+import com.bonsai.accountservice.dto.request.VerifyKYCRequest;
 import com.bonsai.accountservice.dto.response.SuccessResponse;
+import com.bonsai.accountservice.services.KYCService;
 import com.bonsai.accountservice.services.RegistrationService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,33 +15,24 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
+
 @RequiredArgsConstructor
 @Controller
 @Slf4j
-public class KYCController {
+public class KYCFormController {
     private final RegistrationService registrationService;
-
+    private final KYCService kycService;
 
 
     @PostMapping("/registerKYC")
     public ResponseEntity<SuccessResponse> registerKYC(@ModelAttribute RegisterKYCRequest request){
         log.info("Request = {}",request);
         registrationService.saveKYC(request);
-
         return ResponseEntity.ok(
                 new SuccessResponse("saved",true)
         );
     }
 
-//    @PostMapping("/verifyKYC")
-//    public ResponseEntity<SuccessResponse> registerKYC(@ModelAttribute RegisterKYCRequest request){
-//        log.info("Request = {}",request);
-//        registrationService.saveKYC(request);
-//
-//        return ResponseEntity.ok(
-//                new SuccessResponse("saved",true)
-//        );
-//    }
 
 
 }
