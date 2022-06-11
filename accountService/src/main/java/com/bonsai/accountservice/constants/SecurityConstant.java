9e08e0b7055b2628fef283de13acb9c2d070fcb5
nbchand <narenchand02@gmail.com>
2022-06-11 22:17:38 +0545

    refactor: remove unnecessary 'Security Constant'

--- accountService/src/main/java/com/bonsai/accountservice/constants/SecurityConstant.java
+++ accountService/src/main/java/com/bonsai/accountservice/constants/SecurityConstant.java
@@ -10,5 +10,4 @@ public class SecurityConstant {
     public static final long EXPIRATION_TIME = 900_000; // 15 minutes
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/user/sign-up";
 }
