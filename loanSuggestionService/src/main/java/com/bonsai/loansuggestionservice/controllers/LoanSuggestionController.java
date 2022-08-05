94451291a1b0e3aa35f18ad3058233486a467d33
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2022-08-05 22:46:59 +0545

    feat: adds cors in loan suggestion service

--- loanSuggestionService/src/main/java/com/bonsai/loansuggestionservice/controllers/LoanSuggestionController.java
+++ loanSuggestionService/src/main/java/com/bonsai/loansuggestionservice/controllers/LoanSuggestionController.java
@@ -4,6 +4,7 @@ import com.bonsai.loansuggestionservice.services.LoanSuggestionService;
 import com.bonsai.sharedservice.dtos.response.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +18,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
+@CrossOrigin("*")
 public class LoanSuggestionController {
 
     private final LoanSuggestionService loanSuggestionService;
 
     @GetMapping("/getLoanSuggestionsForLender/{email}")
-    public ResponseEntity<SuccessResponse> findAllSuggestedLoansForLender(@PathVariable("email") String email) {
+    public ResponseEntity<SuccessResponse> findAllSuggestedLoansForLender(@PathVariable String email) {
         return ResponseEntity.ok(new SuccessResponse("Suggested loans for borrower fetched successfully",
                 loanSuggestionService.findSuggestedLoansForLender(email)));
     }
