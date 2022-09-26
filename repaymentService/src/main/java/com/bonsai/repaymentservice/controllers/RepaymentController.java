880514541c9b88a0200bfd54bc3a03dfaab67814
808Code <dikshant.shah2k@gmail.com>
2022-09-26 22:18:17 +0545

    setup : added RepaymentController

---
+++ repaymentService/src/main/java/com/bonsai/repaymentservice/controllers/RepaymentController.java
@@ -0,0 +1,16 @@
+package com.bonsai.repaymentservice.controllers;
+
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/")
+@RequiredArgsConstructor
+public class RepaymentController {
+
+
+}
