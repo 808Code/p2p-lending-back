a233320ee0a8a16a176238365f712e109a15ad44
808Code <dikshant.shah2k@gmail.com>
2022-09-27 15:08:29 +0545

    feat . Installment Services

--- repaymentService/src/main/java/com/bonsai/repaymentservice/services/InstallmentService.java
+++ repaymentService/src/main/java/com/bonsai/repaymentservice/services/InstallmentService.java
@@ -1,10 +1,19 @@
 package com.bonsai.repaymentservice.services;
 
+import com.bonsai.repaymentservice.dto.GetInstallmentsDto;
 import com.bonsai.repaymentservice.dto.InstallmentDto;
+import com.bonsai.repaymentservice.dto.InstallmentsResponseDto;
+import com.bonsai.repaymentservice.dto.PayInstallmentRequest;
 
 import java.math.BigDecimal;
 
 public interface InstallmentService {
     void saveInstallment(InstallmentDto installment);
+
     BigDecimal getEmi(long principal, int duration_in_months, float rate_yearly);
+
+    void payInstallment(PayInstallmentRequest payInstallmentRequest);
+
+    InstallmentsResponseDto getInstallments(GetInstallmentsDto getInstallmentsDto);
+
 }
