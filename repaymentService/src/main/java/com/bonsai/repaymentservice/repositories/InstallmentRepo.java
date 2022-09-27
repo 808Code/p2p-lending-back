fd6963e142af1dd3d7de54a240d29207bd1630d8
808Code <dikshant.shah2k@gmail.com>
2022-09-27 15:08:06 +0545

    feat . InstallmentRepo

--- repaymentService/src/main/java/com/bonsai/repaymentservice/repositories/InstallmentRepo.java
+++ repaymentService/src/main/java/com/bonsai/repaymentservice/repositories/InstallmentRepo.java
@@ -4,8 +4,10 @@ import com.bonsai.repaymentservice.models.Installment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface InstallmentRepo extends JpaRepository<Installment, UUID> {
+    List<Installment> findByLoanRequest(UUID id);
 }
