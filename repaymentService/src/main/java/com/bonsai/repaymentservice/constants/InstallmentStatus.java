21841ebd5d58ba32ed2611ef16551d5f2dec922b
808Code <dikshant.shah2k@gmail.com>
2022-09-27 15:09:29 +0545

    refactor : Added Installment status constants

---
+++ repaymentService/src/main/java/com/bonsai/repaymentservice/constants/InstallmentStatus.java
@@ -0,0 +1,8 @@
+package com.bonsai.repaymentservice.constants;
+
+public class InstallmentStatus {
+    public static final String PAID = "PAID";
+    public static final String UNPAID = "UNPAID";
+    public static final String MISSED = "MISSED";
+
+}
