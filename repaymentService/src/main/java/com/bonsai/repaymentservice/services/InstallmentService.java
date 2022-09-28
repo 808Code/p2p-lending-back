f48161b8807e85455482b1fa767fc6059347bf6f
Bimal Raj Gyawali <bimalraj.gyawali@codehimalaya.com>
2022-09-28 21:33:34 +0545

    fix: fixes simple interest calculation

--- repaymentService/src/main/java/com/bonsai/repaymentservice/services/InstallmentService.java
+++ repaymentService/src/main/java/com/bonsai/repaymentservice/services/InstallmentService.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 public interface InstallmentService {
     void saveInstallment(InstallmentDto installment);
 
-    BigDecimal getEmi(long principal, int duration_in_months, float rate_yearly);
+    BigDecimal calculateMonthlyEMI(long principal, int durationInMonths, float rateYearly);
 
     void payInstallment(PayInstallmentRequest payInstallmentRequest);
 
--- repaymentService/src/main/java/com/bonsai/repaymentservice/services/impl/InstallmentServiceImpl.java
+++ repaymentService/src/main/java/com/bonsai/repaymentservice/services/impl/InstallmentServiceImpl.java
@@ -45,9 +45,11 @@ public class InstallmentServiceImpl implements InstallmentService {
     }
 
     @Override
-    public BigDecimal getEmi(long principal, int duration_in_months, float rate_yearly) {
-        BigDecimal emi = BigDecimal.valueOf((principal * duration_in_months * 1.0 * rate_yearly / 1200) + (principal * 1.0 / duration_in_months));
-        return emi;
+    public BigDecimal calculateMonthlyEMI(long principal, int durationInMonths, float rateYearly) {
+        double monthlyInterest =  principal * 1.0 * rateYearly / 1200;
+        double monthlyPrincipal = principal * 1.0 / durationInMonths;
+
+        return BigDecimal.valueOf(monthlyInterest + monthlyPrincipal);
 
 
     }
