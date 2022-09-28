997ecf12088516f07a05e12280c50f1aa4da2160
808Code <dikshant.shah2k@gmail.com>
2022-09-28 17:50:56 +0545

    refactor : Error message

--- repaymentService/src/main/java/com/bonsai/repaymentservice/dto/GetInstallmentsDto.java
+++ repaymentService/src/main/java/com/bonsai/repaymentservice/dto/GetInstallmentsDto.java
@@ -1,5 +1,8 @@
 package com.bonsai.repaymentservice.dto;
 
+import javax.validation.constraints.NotBlank;
+
 public record GetInstallmentsDto (
+        @NotBlank(message = "LoanId cant be Blank.")
         String loanId
 ){}
