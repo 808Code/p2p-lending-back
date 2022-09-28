00590c7789da143885c89e6347d7eb5c7588d644
808Code <dikshant.shah2k@gmail.com>
2022-09-28 17:54:07 +0545

    feat : pay Installment feature

--- repaymentService/src/main/java/com/bonsai/repaymentservice/services/impl/InstallmentServiceImpl.java
+++ repaymentService/src/main/java/com/bonsai/repaymentservice/services/impl/InstallmentServiceImpl.java
@@ -1,14 +1,16 @@
 package com.bonsai.repaymentservice.services.impl;
 
+import com.bonsai.accountservice.models.UserCredential;
+import com.bonsai.accountservice.repositories.UserCredentialRepo;
 import com.bonsai.repaymentservice.constants.InstallmentStatus;
-import com.bonsai.repaymentservice.dto.GetInstallmentsDto;
-import com.bonsai.repaymentservice.dto.InstallmentDto;
-import com.bonsai.repaymentservice.dto.InstallmentsResponseDto;
-import com.bonsai.repaymentservice.dto.PayInstallmentRequest;
+import com.bonsai.repaymentservice.dto.*;
 import com.bonsai.repaymentservice.models.Installment;
 import com.bonsai.repaymentservice.repositories.InstallmentRepo;
+import com.bonsai.repaymentservice.services.DistributionService;
 import com.bonsai.repaymentservice.services.InstallmentService;
 import com.bonsai.sharedservice.exceptions.AppException;
+import com.bonsai.walletservice.constants.WalletTransactionTypes;
+import com.bonsai.walletservice.services.WalletService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,8 +19,8 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -27,6 +29,9 @@ import java.util.UUID;
 public class InstallmentServiceImpl implements InstallmentService {
 
     private final InstallmentRepo installmentRepo;
+    private final WalletService walletService;
+    private final UserCredentialRepo userCredentialRepo;
+    private final DistributionService distributionService;
 
     @Override
     public void saveInstallment(InstallmentDto installment) {
@@ -52,34 +57,59 @@ public class InstallmentServiceImpl implements InstallmentService {
 
     @Override
     public void payInstallment(PayInstallmentRequest payInstallmentRequest) {
-        //check if amount
-        //debit wallet
-        Installment installment = installmentRepo.findById(UUID.fromString(payInstallmentRequest.installmentId()))
-                .orElseThrow(() -> new AppException("Invalid Installment Id", HttpStatus.BAD_REQUEST));
-        log.info("The installment{}", installment);
+        List<Installment> installments = installmentRepo.findAllByLoanRequest(UUID.fromString(payInstallmentRequest.loanId()));
+
+        Iterator<Installment> installmentIterator = installments.iterator();
+        Installment installment = null;
+        int paidCounter = 0;
+        while (installmentIterator.hasNext()) {
+            Installment tempInstallment = installmentIterator.next();
+            if (tempInstallment.getId().equals(UUID.fromString(payInstallmentRequest.installmentId()))) {
+                installment = tempInstallment;
+            }
+            if (tempInstallment.getStatus().equals(InstallmentStatus.PAID)) {
+                paidCounter++;
+            }
+        }
+        if (installment == null) {
+            throw new AppException("Invalid Installment payment request", HttpStatus.BAD_REQUEST);
+        }
+
+        walletService.debitOrLockAmount(WalletTransactionTypes.DEBIT, installment.getAmount(), payInstallmentRequest.email());
         installment.setStatus(InstallmentStatus.PAID);
         installment.setPaidDate(LocalDate.now());
         installmentRepo.save(installment);
+        log.info("THIS Loan ID {}",payInstallmentRequest.loanId());
+        distributionService.distribute(
+                installmentRepo.fetchAllByLoanRequestIdTheEmailAndAmount(UUID.fromString(payInstallmentRequest.loanId()))
+                ,installment.getAmount());
+
+        if (installments.size() - paidCounter == 1) {
+            UserCredential userCredential = userCredentialRepo.findByEmail(payInstallmentRequest.email())
+                    .orElseThrow(() -> new AppException("Email not found in database.", HttpStatus.NOT_FOUND));
+            userCredential.setOngoingLoan(false);
+            userCredentialRepo.save(userCredential);
+
+        }
+
 
     }
 
     @Override
     public InstallmentsResponseDto getInstallments(GetInstallmentsDto getInstallmentsDto) {
-        List<Installment> installments = installmentRepo.findByLoanRequest(UUID.fromString(getInstallmentsDto.loanId()));
+        List<Installment> installments = installmentRepo.findAllByLoanRequest(UUID.fromString(getInstallmentsDto.loanId()));
         List<Installment> paidInstallment = new ArrayList<>();
         List<Installment> unpaidInstallment = new ArrayList<>();
         List<Installment> missedInstallment = new ArrayList<>();
         for (Installment installment : installments) {
-            if (Objects.equals(installment.getStatus(), InstallmentStatus.PAID)) {
+            if (installment.getStatus().equals(InstallmentStatus.PAID)) {
                 paidInstallment.add(installment);
+            } else if ((installment.getScheduledDate()).isBefore(LocalDate.now())) {
+                missedInstallment.add(installment);
             } else {
-                if ((installment.getScheduledDate()).isBefore(LocalDate.now())) {
-                    missedInstallment.add(installment);
-                }else{
-                    unpaidInstallment.add(installment);
-                }
-
+                unpaidInstallment.add(installment);
             }
+
         }
 
 
