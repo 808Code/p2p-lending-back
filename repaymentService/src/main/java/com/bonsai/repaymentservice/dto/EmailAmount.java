34fe44f6c2479b02f41d757f02bab329204e160d
808Code <dikshant.shah2k@gmail.com>
2022-09-28 17:50:05 +0545

    feat : Distribute installment

--- repaymentService/src/main/java/com/bonsai/repaymentservice/dto/EmailAmount.java
+++ repaymentService/src/main/java/com/bonsai/repaymentservice/dto/EmailAmount.java
@@ -1,14 +1,9 @@
 package com.bonsai.repaymentservice.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+public interface EmailAmount {
+    String getEmail();
 
-
-public interface EmailAmount{
-     String getEmail();
-     Long getAmount();
+    Long getAmount();
 
 
 }
