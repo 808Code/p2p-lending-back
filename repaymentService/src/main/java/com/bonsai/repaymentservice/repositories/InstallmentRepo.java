887744bb2e547075814525c90157758bbd17cfd1
808Code <dikshant.shah2k@gmail.com>
2022-09-30 10:53:31 +0545

    refactor : used cast on native query

--- repaymentService/src/main/java/com/bonsai/repaymentservice/repositories/InstallmentRepo.java
+++ repaymentService/src/main/java/com/bonsai/repaymentservice/repositories/InstallmentRepo.java
@@ -1,6 +1,6 @@
 package com.bonsai.repaymentservice.repositories;
 
-import com.bonsai.repaymentservice.dto.EmailAmount;
+import com.bonsai.repaymentservice.dto.Lending;
 import com.bonsai.repaymentservice.models.Installment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -14,10 +14,13 @@ public interface InstallmentRepo extends JpaRepository<Installment, UUID> {
     List<Installment> findAllByLoanRequest(UUID loanRequestId);
 
     @Query(nativeQuery = true, value = """
-            select email,amount 
-            from user_credential 
-            INNER JOIN(SELECT  lender_id,amount,loan_request_id FROM lending l
-            INNER JOIN wallet_transaction wt on l.transaction_id = wt.id)lt on user_credential.id=lt.lender_id where loan_request_id = ?1  
-            """)
-    List<EmailAmount> fetchAllByLoanRequestIdTheEmailAndAmount(UUID loanRequestId);
+        select email, amount, CAST(lender_id as varchar) id
+        from user_credential
+                 INNER JOIN(SELECT lender_id, amount, loan_request_id, l.id as lending_id
+                            FROM lending l
+                                     INNER JOIN wallet_transaction wt on l.transaction_id = wt.id) lt
+                           on user_credential.id = lt.lender_id
+        where loan_request_id = ?1
+         """)
+    List<Lending> fetchAllByLoanRequestIdTheEmailAndAmount(UUID loanRequestId);
 }
