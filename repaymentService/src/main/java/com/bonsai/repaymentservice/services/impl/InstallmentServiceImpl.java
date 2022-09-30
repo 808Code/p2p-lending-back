452dfdd411a45f41ea9031cc7a6336e80a3938f6
808Code <dikshant.shah2k@gmail.com>
2022-09-30 11:04:43 +0545

    feat: pay installment feature

--- repaymentService/src/main/java/com/bonsai/repaymentservice/services/impl/InstallmentServiceImpl.java
+++ repaymentService/src/main/java/com/bonsai/repaymentservice/services/impl/InstallmentServiceImpl.java
@@ -5,7 +5,10 @@ import com.bonsai.accountservice.repositories.UserCredentialRepo;
 import com.bonsai.repaymentservice.constants.InstallmentStatus;
 import com.bonsai.repaymentservice.dto.*;
 import com.bonsai.repaymentservice.models.Installment;
+import com.bonsai.repaymentservice.models.PlatformEarning;
 import com.bonsai.repaymentservice.repositories.InstallmentRepo;
+import com.bonsai.repaymentservice.repositories.InterestDistributionRepo;
+import com.bonsai.repaymentservice.repositories.PlatformEarningRepo;
 import com.bonsai.repaymentservice.services.DistributionService;
 import com.bonsai.repaymentservice.services.InstallmentService;
 import com.bonsai.sharedservice.exceptions.AppException;
@@ -32,6 +35,8 @@ public class InstallmentServiceImpl implements InstallmentService {
     private final WalletService walletService;
     private final UserCredentialRepo userCredentialRepo;
     private final DistributionService distributionService;
+    private final PlatformEarningRepo platformEarningRepo;
+
 
     @Override
     public void saveInstallment(InstallmentDto installment) {
@@ -79,10 +84,20 @@ public class InstallmentServiceImpl implements InstallmentService {
         installment.setStatus(InstallmentStatus.PAID);
         installment.setPaidDate(LocalDate.now());
         installmentRepo.save(installment);
-        log.info("THIS Loan ID {}",payInstallmentRequest.loanId());
-        distributionService.distribute(
-                installmentRepo.fetchAllByLoanRequestIdTheEmailAndAmount(UUID.fromString(payInstallmentRequest.loanId()))
-                ,installment.getAmount());
+        List<Lending> lendings = installmentRepo.fetchAllByLoanRequestIdTheEmailAndAmount(UUID.fromString(payInstallmentRequest.loanId()));
+
+        //remaining amount only from emi not principal
+        BigDecimal remainingAmount = distributionService.distribute(
+                lendings,
+                installment.getAmount(),
+                installments.size()
+        );
+        platformEarningRepo.save(
+                PlatformEarning.builder()
+                        .amount(remainingAmount)
+                        .installment(installment)
+                        .build()
+        );
 
         if (installments.size() - paidCounter == 1) {
             UserCredential userCredential = userCredentialRepo.findByEmail(payInstallmentRequest.email())
