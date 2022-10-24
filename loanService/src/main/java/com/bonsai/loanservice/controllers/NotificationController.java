6df4637c2b9e2d2f02230d2c769df161717c78d4
Bimal Raj Gyawali <gyawali.rajbimal35@gmail.com>
2022-10-24 16:31:13 +0545

    refactor: remove test method

--- loanService/src/main/java/com/bonsai/loanservice/controllers/NotificationController.java
+++ loanService/src/main/java/com/bonsai/loanservice/controllers/NotificationController.java
@@ -1,6 +1,5 @@
 package com.bonsai.loanservice.controllers;
 
-import com.bonsai.loanservice.dto.Notification;
 import com.bonsai.loanservice.services.NotificationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,8 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import java.io.IOException;
 
 @RestController
 @RequestMapping("sse")
@@ -24,11 +21,5 @@ public class NotificationController {
     public SseEmitter sseEmitter(@PathVariable String email) {
       return notificationService.addSubscription(email);
     }
-
-    @GetMapping("/test")
-    public void test() {
-         notificationService.pushNotification(new Notification("borrower@gmail.com", "Loan fulfilled"));
-    }
-
 }
 
